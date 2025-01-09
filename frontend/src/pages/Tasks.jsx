b76e5f3a58dbf9c688f91b7061c79f612c5ada2a
Madiha <madeeha.hiba123@gmail.com>
2025-01-10 00:25:32 +0500

    Task conflict fix

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-=======
-// File: src/App.jsx
->>>>>>> 501179538b1d0cd2c8180055bef388bcb0ad9acc
+
 import React from "react";
 import TaskCards from "../components/tasks/TaskCards";
 import TaskKanban from "../components/tasks/TaskKanban";
 import { Outlet } from "react-router-dom";
 
-<<<<<<< HEAD
-const Tasks = () => {
-  return (
-    <div className="bg-white-100 min-h-screen">
-      <TaskCards />
-      <div className="bg-white-100 min-h-screen">
-        <TaskKanban />
-        <Outlet />
-      </div>
-    </div>
-=======
 import EmployeeSidebar from "../components/dashboard/employeeSidebar";
 
 import { Navigate } from "react-router-dom";
@@ -40,7 +26,6 @@ const Tasks = () => {
         <Outlet/>
       </div>
     // </div>
->>>>>>> 501179538b1d0cd2c8180055bef388bcb0ad9acc
   );
 };
 
