92084f9f172223a5c349a2581c9c614b4b8c1ede
Madiha <madeeha.hiba123@gmail.com>
2025-01-10 22:14:00 +0500

    AttendanceManagemnt fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,48 +31,40 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/admin-dashboard" />} />
-        <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* Admin Dashboard */}
-        <Route path="/admin-dashboard" element={<AdminDashboard />}>
-          <Route index element={<AdminSummary />} />
-          <Route path="/admin-dashboard/employee" element={<List />} />
-          <Route path="/admin-dashboard/add-employee" element={<Add />} />
-          <Route path="/admin-dashboard/edit-employee/:id" element={<EditEmployee />} />
-          <Route path="/admin-dashboard/view-employee/:id" element={<ViewEmployee />} />
-          <Route path="/admin-dashboard/attend-manage" element={<AttendanceManag />} />
-          <Route path="/admin-dashboard/payroll" element={<Payroll />} />
-          <Route path="/admin-dashboard/tasks" element={<AdminTaskList/>}/>
-          <Route path="/admin-dashboard/create-task" element={<CreateTask/>}/>
-          <Route path="/admin-dashboard/settings" element={<Setting />} />
-        </Route>
+            {/* Admin Dashboard */}
+            <Route path="/admin-dashboard" element={<AdminDashboard />}>
+              <Route index element={<AdminSummary />} />
+              <Route path="/admin-dashboard/employee" element={<List />} />
+              <Route path="/admin-dashboard/add-employee" element={<Add />} />
+              <Route path="/admin-dashboard/edit-employee/:id" element={<EditEmployee />} />
+              <Route path="/admin-dashboard/view-employee/:id" element={<ViewEmployee />} />
+              <Route path="/admin-dashboard/attend-manage" element={<AttendanceManag />} />
+              <Route path="/admin-dashboard/payroll" element={<Payroll />} />
+              <Route path="/admin-dashboard/tasks" element={<AdminTaskList/>}/>
+              <Route path="/admin-dashboard/create-task" element={<CreateTask/>}/>
+              <Route path="/admin-dashboard/settings" element={<Setting />} />
+            </Route>
 
-        {/* Employee Dashboard */}
-        <Route path="/employee-dashboard" element={<EmployeeDashboard />}>
-          <Route index element={<EmployeeSummary />} />
-          <Route path="/employee-dashboard/tasks" element={<Tasks />} />
-          <Route path="/employee-dashboard/settings" element={<Setting />} />
-          <Route path="/employee-dashboard/payroll" element={<EmployeePayroll />} />
-          <Route path="/employee-dashboard/attendance-and-leaves" element={<EmployeeAttendance />} />
-        </Route>
+            {/* Employee Dashboard */}
+            <Route path="/employee-dashboard" element={<EmployeeDashboard />}>
+              <Route index element={<EmployeeSummary />} />
+              <Route path="/employee-dashboard/tasks" element={<Tasks />} />
+              <Route path="/employee-dashboard/settings" element={<Setting />} />
+              <Route path="/employee-dashboard/payroll" element={<EmployeePayroll />} />
+              <Route path="/employee-dashboard/attendance-and-leaves" element={<EmployeeAttendance />} />
+            </Route>
 
 
-        {/* TeamLead Dashboard */}
-        <Route path="/tl-dashboard" element={<TLDashboard />}>
-          <Route index element={<EmployeeSummary />} />
-          <Route path="/tl-dashboard/tasks" element={<Tasks />} />
-          <Route path="/tl-dashboard/settings" element={<Setting />} />
-          <Route path="/tl-dashboard/payroll" element={<EmployeePayroll />} />
-          <Route path="/tl-dashboard/attendance-and-leaves" element={<EmployeeAttendance />} />
-        </Route>
-
-        
-        
-        
-          
-            
-         
-        
+            {/* TeamLead Dashboard */}
+            <Route path="/tl-dashboard" element={<TLDashboard />}>
+              <Route index element={<EmployeeSummary />} />
+              <Route path="/tl-dashboard/tasks" element={<Tasks />} />
+              <Route path="/tl-dashboard/settings" element={<Setting />} />
+              <Route path="/tl-dashboard/payroll" element={<EmployeePayroll />} />
+              <Route path="/tl-dashboard/attendance-and-leaves" element={<EmployeeAttendance />} />
+            </Route>
       </Routes>
     </BrowserRouter>
   );
