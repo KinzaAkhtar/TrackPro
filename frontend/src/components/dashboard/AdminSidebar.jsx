c24813c6724fedab5f9203d7ef6c07147db08911
Madiha <madeeha.hiba123@gmail.com>
2025-01-11 14:25:09 +0500

    Admin Sidebar update

--- frontend/src/components/dashboard/AdminSidebar.jsx
+++ frontend/src/components/dashboard/AdminSidebar.jsx
@@ -47,10 +47,6 @@ const AdminSidebar = () => {
                     <FaDollarSign className="mr-2" />
                     <span>Payroll</span>
                 </NavLink>
-                <NavLink to="admin-dashboard/performance" className="flex items-center space-x-4 block py-2.5 px-4 hover:bg-yellow-600 rounded-lg">
-                    <FaTachometerAlt className="mr-2" />
-                    <span>Performance Monitoring</span>
-                </NavLink>
                 <NavLink to="/admin-dashboard/settings" className={({ isActive }) => `${isActive ? "bg-white bg-opacity-20" : " "} flex items-center space-x-4 block py-2.5 px-4 hover:bg-yellow-600 rounded-lg`}>
                     <FaCog className="mr-2" />
                     <span>Settings</span>
